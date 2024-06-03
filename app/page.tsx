bc2a6207a1f6a9691ca7fa33fa15ecdb639e4f4b
Othman-Elsayed <sam44104@gmail.com>
2024-06-03 23:37:33 +0300

    update 5

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="versionContainer">
           <div className="title">Downloads Files</div>
           <a href={"./TRUST-APP-AND.apk"} download className="box">
-            <div className="title">Android new version (4)</div>
+            <div className="title">Android new version (5)</div>
             <div className="img">
               <Image
                 width={100}
